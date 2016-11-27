112c2b36c320b06ac3b65d17b18091ef465db870
Malte Ubl <malte.ubl@gmail.com>
2016-11-26 23:14:15 -0800

    Add export to make bundling into N modules successful.

--- sample/lib/react.js
+++ sample/lib/react.js
@@ -2,6 +2,8 @@ var ReactDOMServer = require('react-dom/server');
 var React = require('react');
 import {Greeting} from './react-component';
 
-
 console.log(
     ReactDOMServer.renderToString(<Greeting name="Splittable in React" />));
+
+// Currently bundle root modules must have at least one export.
+export function anything() {}
